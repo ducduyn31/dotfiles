3602955144240e74600ab4e9ecd301d068a07b2c
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-05 11:27:07 +1100

    add mkalias to fix splotlight issue

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,63 +1,81 @@
 {
-	description = "Daniel's Darwin system flake";
+  description = "Daniel's Darwin system flake";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-		nix-darwin.url = "github:LnL7/nix-darwin";
-		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-	outputs = inputs@{ self, nix-darwin, nixpkgs }:
-	let
-		configuration = { pkgs, config, ... }: {
-			# List packages installed in system profile. To search by name, run:
-			# $ nix-env -qaP | grep wget
-			environment.systemPackages = [ 
-				pkgs.vim
-				pkgs.neovim
-				pkgs.tmux
-				pkgs.alacritty
-			];
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [ 
+        pkgs.vim
+        pkgs.mkalias
+        pkgs.neovim
+        pkgs.tmux
+        pkgs.alacritty
+      ];
 
-			fonts.packages = [
-				(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })		
-			]
-	
-			# Auto upgrade nix package and the daemon service.
-			services.nix-daemon.enable = true;
-			# nix.package = pkgs.nix;
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      ];
 
-			# Necessary for using flakes on this system.
-			nix.settings.experimental-features = "nix-command flakes";
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
 
-			# Create /etc/zshrc that loads the nix-darwin environment.
-			programs.zsh.enable = true;	# default shell on catalina
-			# programs.fish.enable = true;
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
 
-			# Set Git commit hash for darwin-version.
-			system.configurationRevision = self.rev or self.dirtyRev or null;
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
-			# Used for backwards compatibility, please read the changelog before changing.
-			# $ darwin-rebuild changelog
-			system.stateVersion = 5;
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true; # default shell on catalina
 
-			# The platform the configuration will be used on.
-			nixpkgs.hostPlatform = "aarch64-darwin";
-	
-			# Add a shell alias for darwin-rebuild switch.
-			environment.shellAliases = {
-				dswitch = "darwin-rebuild switch --flake ${self}#daniel";
-			};
-		};
-	in
-	{
-		# Build darwin flake using:
-		# $ darwin-rebuild build --flake .#daniel
-		darwinConfigurations."daniel" = nix-darwin.lib.darwinSystem {
-			modules = [ configuration ];
-		};
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-		# Expose the package set, including overlays, for convenience.
-		darwinPackages = self.darwinConfigurations."daniel".pkgs;
-	};
+      # Used for backwards compatibility, please read the changelog before changing.
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Add a shell alias for darwin-rebuild switch.
+      environment.shellAliases = {
+        dswitch = "darwin-rebuild switch --flake ~/.dotfiles/nix-config#daniel";
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#daniel
+    darwinConfigurations."daniel" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."daniel".pkgs;
+  };
 }
+
