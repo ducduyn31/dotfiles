304ec75ae8b0a1a51581acc31780df29d2527430
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-05 11:07:07 +1100

    add neovim, tmux, alacritty

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,52 +1,63 @@
 {
-  description = "Daniel Darwin system flake";
+	description = "Daniel's Darwin system flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nix-darwin.url = "github:LnL7/nix-darwin";
+		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+	};
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-			pkgs.neovim
-        ];
+	outputs = inputs@{ self, nix-darwin, nixpkgs }:
+	let
+		configuration = { pkgs, config, ... }: {
+			# List packages installed in system profile. To search by name, run:
+			# $ nix-env -qaP | grep wget
+			environment.systemPackages = [ 
+				pkgs.vim
+				pkgs.neovim
+				pkgs.tmux
+				pkgs.alacritty
+			];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+			fonts.packages = [
+				(pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })		
+			]
+	
+			# Auto upgrade nix package and the daemon service.
+			services.nix-daemon.enable = true;
+			# nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+			# Necessary for using flakes on this system.
+			nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+			# Create /etc/zshrc that loads the nix-darwin environment.
+			programs.zsh.enable = true;	# default shell on catalina
+			# programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+			# Set Git commit hash for darwin-version.
+			system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+			# Used for backwards compatibility, please read the changelog before changing.
+			# $ darwin-rebuild changelog
+			system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#daniel
-    darwinConfigurations."daniel" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
+			# The platform the configuration will be used on.
+			nixpkgs.hostPlatform = "aarch64-darwin";
+	
+			# Add a shell alias for darwin-rebuild switch.
+			environment.shellAliases = {
+				dswitch = "darwin-rebuild switch --flake ${self}#daniel";
+			};
+		};
+	in
+	{
+		# Build darwin flake using:
+		# $ darwin-rebuild build --flake .#daniel
+		darwinConfigurations."daniel" = nix-darwin.lib.darwinSystem {
+			modules = [ configuration ];
+		};
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."daniel".pkgs;
-  };
+		# Expose the package set, including overlays, for convenience.
+		darwinPackages = self.darwinConfigurations."daniel".pkgs;
+	};
 }
