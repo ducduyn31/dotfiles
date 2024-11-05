47e6d53ca530f82e024b6dfa828eb0a206120cb1
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-05 13:57:30 +1100

    add backup file extension for home-manager

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -110,6 +110,7 @@
 		home-manager.darwinModules.home-manager {
 		  home-manager.useGlobalPkgs = true;
 		  home-manager.useUserPackages = true;
+		  home-manager.backupFileExtension = "backup";
 		  home-manager.users.danielng = import ./danielng.nix;
 		}
 	  ];
