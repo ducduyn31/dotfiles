26d442ebb3b0b6983c179a772e8e36e366dab5e7
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-05 22:18:18 +1100

    update alacritty config

--- nix-config/danielng.nix
+++ nix-config/danielng.nix
@@ -19,6 +19,18 @@
       font = {
         size = 16;
       };
+      window = {
+        opacity = 0.75;
+        padding = {
+          x = 5;
+          y = 10;
+        };
+        decorations = "Transparent";
+        startup_mode = "SimpleFullscreen";
+      };
+      colors = {
+        transparent_background_colors = true;
+      };
       selection.save_to_clipboard = true;
     };
   };
--- nix-config/nixvim-config/default.nix
+++ nix-config/nixvim-config/default.nix
@@ -32,13 +32,10 @@
   #
   # If you want to see what colorschemes are already installed, you can use `:Telescope colorschme`.
   colorschemes = {
-    # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-    tokyonight = {
+    rose-pine = {
       enable = true;
       settings = {
-        # Like many other themes, this one has different styles, and you could load
-        # any other, such as 'storm', 'moon', or 'day'.
-        style = "night";
+        transparency = true;
       };
     };
   };
