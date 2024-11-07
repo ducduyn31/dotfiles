71dd80e795988cfd10b3281185c2bbfba888713f
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-07 13:28:16 +1100

    add another workspace for aerospace

--- nix-config/home-manager/aerospace.nix
+++ nix-config/home-manager/aerospace.nix
@@ -32,11 +32,13 @@
       alt-2 = 'workspace 2'
       alt-3 = 'workspace 3'
       alt-4 = 'workspace 4'
+      alt-5 = 'workspace 4'
 
       alt-shift-1 = 'move-node-to-workspace 1'
       alt-shift-2 = 'move-node-to-workspace 2'
       alt-shift-3 = 'move-node-to-workspace 3'
       alt-shift-4 = 'move-node-to-workspace 4'
+      alt-shift-5 = 'move-node-to-workspace 5'
 
       alt-shift-c = 'reload-config'
 
@@ -44,15 +46,20 @@
       1 = 'main'
       2 = 'main'
       3 = 'main'
-      4 = ['msi', 'dell', 'builtin', 'secondary', 'main']
+      4 = 'main'
+      5 = ['msi', 'dell', 'builtin', 'secondary', 'main']
 
       [[on-window-detected]]
       if.app-name-regex-substring = 'alacritty'
       run = 'move-node-to-workspace 2'
 
       [[on-window-detected]]
+      if.app-name-regex-substring = 'freeform'
+      run = 'move-node-to-workspace 3'
+
+      [[on-window-detected]]
       if.app-name-regex-substring = 'arc'
-      run = 'move-node-to-workspace 4'
+      run = 'move-node-to-workspace 5'
     '';
   };
 }
