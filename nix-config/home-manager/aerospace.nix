a39249cf0c59d68f5cf66e35eec51255f5ffc735
ducduyn31 <ducduy.ababat@gmail.com>
2024-11-15 23:28:41 +1100

    update aerospace config

--- nix-config/home-manager/aerospace.nix
+++ nix-config/home-manager/aerospace.nix
@@ -32,7 +32,7 @@
       alt-2 = 'workspace 2'
       alt-3 = 'workspace 3'
       alt-4 = 'workspace 4'
-      alt-5 = 'workspace 4'
+      alt-5 = 'workspace 5'
 
       alt-shift-1 = 'move-node-to-workspace 1'
       alt-shift-2 = 'move-node-to-workspace 2'
