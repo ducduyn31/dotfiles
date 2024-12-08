261a7bbcfd264e29f6bc5f316e634510aa22d716
ducduyn31 <ducduy.ababat@gmail.com>
2024-12-08 19:24:30 +1100

    separate rose profile

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -35,6 +35,10 @@
       };
       system = "aarch64-darwin";
 
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      # A helper to generate attrset { x86_64-linux = ...; aarch64-darwin = ...; ... }
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
       pkgs = import nixpkgs {
         config.allowUnfree = true;
         system = system;
@@ -53,97 +57,13 @@
         default = nvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
       };
 
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt;
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
       # Build darwin flake using:
       # $ darwin-rebuild switch --flake .#rose
-      darwinConfigurations.rose = nix-darwin.lib.darwinSystem {
-        inherit system;
-
-        # Make all inputs available in imported files
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          ./darwin
-          ({ pkgs, inputs, ... }: {
-            nixpkgs.config = nixpkgsConfig;
-
-            environment.systemPackages = [
-              nvim
-              # GUI apps
-              pkgs.alacritty
-              pkgs.mkalias
-              pkgs.obsidian
-              pkgs.appcleaner
-            ];
-
-            # The platform the configuration will be used on.
-            nixpkgs.hostPlatform = system;
-
-            system = {
-              stateVersion = 5;
-              configurationRevision = self.rev or self.dirtyRev or null;
-            };
-
-            # Set the home directory
-            users.users.${globals.user} = {
-              home = "/Users/${globals.user}";
-              shell = pkgs.zsh;
-            };
-
-            networking = {
-              computerName = globals.macHostname;
-              hostName = globals.macHostname;
-              localHostName = globals.macHostname;
-            };
-
-            nix = {
-              # Enable flakes
-              gc = {
-                automatic = false;
-                user = globals.user;
-              };
-
-              settings = {
-                allowed-users = [ globals.user ];
-                experimental-features = "flakes nix-command";
-                warn-dirty = false;
-              };
-            };
-          })
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                inherit inputs;
-                pkgs-zsh-fzf-tab =
-                  import inputs.nixpkgs-zsh-fzf-tab { inherit system; };
-              };
-              users.${globals.user} = { ... }:
-                with inputs; {
-                  imports = [ ./home-manager ./shell ];
-                  home.username = globals.user;
-                  home.stateVersion = "24.05";
-                };
-            };
-          }
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              # Install Homebew under the default prefix
-              enable = true;
-
-              # Apple Sillicon config
-              enableRosetta = true;
-              user = globals.user;
-
-              # Automatically migrate existing Homebew installations
-              autoMigrate = true;
-            };
-          }
-        ];
+      darwinConfigurations = {
+        rose =
+          import ./profiles/rose { inherit inputs globals nixpkgsConfig nvim; };
       };
     };
 }
---
+++ nix-config/profiles/rose/default.nix
@@ -0,0 +1,92 @@
+{ inputs, nixpkgsConfig, globals, nvim }:
+
+with inputs;
+nix-darwin.lib.darwinSystem {
+  system = "aarch64-darwin";
+
+  # Make all inputs available in imported files
+  specialArgs = { inherit inputs; };
+
+  modules = [
+    ../../darwin
+    ({ pkgs, inputs, ... }: {
+      nixpkgs.config = nixpkgsConfig;
+
+      environment.systemPackages = [
+        nvim
+        # GUI apps
+        pkgs.alacritty
+        pkgs.mkalias
+        pkgs.obsidian
+        pkgs.appcleaner
+      ];
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      system = {
+        stateVersion = 5;
+        configurationRevision = self.rev or self.dirtyRev or null;
+      };
+
+      # Set the home directory
+      users.users.${globals.user} = {
+        home = "/Users/${globals.user}";
+        shell = pkgs.zsh;
+      };
+
+      networking = {
+        computerName = globals.macHostname;
+        hostName = globals.macHostname;
+        localHostName = globals.macHostname;
+      };
+
+      nix = {
+        # Enable flakes
+        gc = {
+          automatic = false;
+          user = globals.user;
+        };
+
+        settings = {
+          allowed-users = [ globals.user ];
+          experimental-features = "flakes nix-command";
+          warn-dirty = false;
+        };
+      };
+    })
+    home-manager.darwinModules.home-manager
+    {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        backupFileExtension = "backup";
+        extraSpecialArgs = {
+          inherit inputs;
+          pkgs-zsh-fzf-tab =
+            import inputs.nixpkgs-zsh-fzf-tab { system = "aarch64-darwin"; };
+        };
+        users.${globals.user} = { ... }:
+          with inputs; {
+            imports = [ ../../home-manager ../../shell ];
+            home.username = globals.user;
+            home.stateVersion = "24.05";
+          };
+      };
+    }
+    nix-homebrew.darwinModules.nix-homebrew
+    {
+      nix-homebrew = {
+        # Install Homebew under the default prefix
+        enable = true;
+
+        # Apple Sillicon config
+        enableRosetta = true;
+        user = globals.user;
+
+        # Automatically migrate existing Homebew installations
+        autoMigrate = true;
+      };
+    }
+  ];
+}
