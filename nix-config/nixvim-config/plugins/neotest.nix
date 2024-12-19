23e14d28730365860feac0af185dc01734457e43
ducduyn31 <ducduy.ababat@gmail.com>
2024-12-19 22:39:43 +1100

    fix neotest commands

--- nix-config/nixvim-config/plugins/neotest.nix
+++ nix-config/nixvim-config/plugins/neotest.nix
@@ -23,55 +23,55 @@
     {
       mode = "n";
       key = "<leader>tt";
-      action = ":lua require'neotest'.run.run()<CR>";
+      action = ":lua require('neotest').run.run()<CR>";
       options = { desc = "[T]est [T]his function"; };
     }
     {
       mode = "n";
       key = "<leader>tT";
-      action = ":lua require'neotest'.run.run(vim.uv.cwd())<CR>";
+      action = ":lua require('neotest').run.run(vim.uv.cwd())<CR>";
       options = { desc = "[T]est All"; };
     }
     {
       mode = "n";
       key = "<leader>tl";
-      action = ":lua require'neotest'.run.run_last()<CR>";
+      action = ":lua require('neotest').run.run_last()<CR>";
       options = { desc = "[T]est [L]ast"; };
     }
     {
       mode = "n";
       key = "<leader>tf";
-      action = ":lua require'neotest'.run.run_file()<CR>";
+      action = ":lua require('neotest').run.run(vim.fn.expand('%))<CR>";
       options = { desc = "[T]est [F]ile"; };
     }
     {
       mode = "n";
       key = "<leader>ts";
-      action = ":lua require'neotest'.run.run_suite()<CR>";
+      action = ":lua require('neotest').run.run_suite()<CR>";
       options = { desc = "[T]est [S]uite"; };
     }
     {
       mode = "n";
       key = "<leader>td";
-      action = ":lua require'neotest'.run.debug()<CR>";
+      action = ":lua require('neotest').run.debug()<CR>";
       options = { desc = "[T]est [D]ebug"; };
     }
     {
       mode = "n";
       key = "<leader>ts";
-      action = ":lua require'neotest'.summary.toggle()<CR>";
+      action = ":lua require('neotest').summary.toggle()<CR>";
       options = { desc = "Toggle [T]est [S]ummary"; };
     }
     {
       mode = "n";
       key = "<leader>to";
-      action = ":lua require'neotest'.output_panel.toggle()<CR>";
+      action = ":lua require('neotest').output_panel.toggle()<CR>";
       options = { desc = "Toggle [T]est [O]utput"; };
     }
     {
       mode = "n";
       key = "<leader>dm";
-      action = ":lua require'neotest'.run.run({ strategy = 'dap' })<CR>";
+      action = ":lua require('neotest').run.run({ strategy = 'dap' })<CR>";
       options = { desc = "[D]ebug current [M]ethod"; };
     }
   ];
