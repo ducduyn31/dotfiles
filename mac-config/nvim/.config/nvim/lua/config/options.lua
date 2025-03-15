ef7a14153aa77407fcf1e21e36ec0bc17a70cff1
ducduyn31 <ducduy.ababat@gmail.com>
2025-03-15 21:50:08 +1100

    use rust-analyzer

--- mac-config/nvim/.config/nvim/lua/config/options.lua
+++ mac-config/nvim/.config/nvim/lua/config/options.lua
@@ -6,7 +6,7 @@ vim.g.lazyvim_cmp = "blink.cmp"
 vim.g.snacks_animate = false
 
 -- Rust
-vim.g.lazyvim_rust_diagnostics = "bacon-ls"
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 
 -- Python
 vim.g.lazyvim_python_ruff = "ruff"
